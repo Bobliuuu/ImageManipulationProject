1c3ff629c65fdec28b436bb0d499a0f4dd7c575c
Daniel Qian <95648807+danielqian0922@users.noreply.github.com>
2021-12-09 16:32:42 -0500

    Highlighted Colour Buttons

--- Group#3Image/ColorButton.java
+++ Group#3Image/ColorButton.java
@@ -6,14 +6,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ColorButton extends Actor
+public class ColorButton extends Control
 {
     private GreenfootImage image;
+    private Color color;
     
     /**
      * Basic constructor to create a color button
      */
     public ColorButton(Color colour) {
+        color = colour;
         image = new GreenfootImage(20, 20);
         int borderThickness = 2;
         Color borderColor = Color.BLACK;
@@ -23,13 +25,16 @@ public class ColorButton extends Actor
         }
         image.setColor(colour);
         image.fillRect(2, 2, 16, 16);
+        mouseOver = false;
+        clickable = true;
         setImage(image);
     }
     
     /**
      * Similar to above, but with the ability to adjust width and height
      */
-    public ColorButton(Color colour, int width, int height) {
+    public ColorButton(Color colour, int width, int height, boolean isClickable) {
+        color = colour;
         image = new GreenfootImage(width, height);
         int borderThickness = 2;
         Color borderColor = Color.BLACK;
@@ -39,13 +44,16 @@ public class ColorButton extends Actor
         }
         image.setColor(colour);
         image.fillRect(borderThickness, borderThickness, width - borderThickness, height - borderThickness);
+        mouseOver = false;
+        clickable = isClickable;
         setImage(image);
     } 
     
     /**
      * Similar to above, but with the ability to adjust border thickness
      */
-    public ColorButton(Color colour, int width, int height, int borderThickness) {
+    public ColorButton(Color colour, int width, int height, int borderThickness, boolean isClickable) {
+        color = colour;
         image = new GreenfootImage(width, height);
         Color borderColor = Color.BLACK;
         image.setColor (borderColor);
@@ -54,13 +62,16 @@ public class ColorButton extends Actor
         }
         image.setColor(colour);
         image.fillRect(borderThickness, borderThickness, width - borderThickness, height - borderThickness);
+        mouseOver = false;
+        clickable = isClickable;
         setImage(image);
     } 
     
     /**
      * Similar to above, but with the ability to adjust border color
      */
-    public ColorButton(Color colour, int width, int height, int borderThickness, Color borderColor) {
+    public ColorButton(Color colour, int width, int height, int borderThickness, Color borderColor, boolean isClickable) {
+        color = colour;
         image = new GreenfootImage(width, height);
         image.setColor (borderColor);
         for (int i = 0; i < borderThickness; i++){
@@ -68,6 +79,52 @@ public class ColorButton extends Actor
         }
         image.setColor(colour);
         image.fillRect(borderThickness, borderThickness, width - borderThickness, height - borderThickness);
+        mouseOver = false;
+        clickable = isClickable;
         setImage(image);
     } 
+    
+    public void update() {
+        highlightedImage = drawTextBox (color, color, Color.WHITE, 2);
+        myImage = image;
+    }
+    
+    /**
+     * 
+     * Draw a text box.
+     * 
+     */
+    public GreenfootImage drawTextBox (Color color, Color back, Color border, int pad){
+        int height = image.getHeight() + (2 * pad);
+        int width = image.getWidth() + (2 * pad);
+        
+        // Ensure an even width to keep text properly centered
+        if (width % 2 == 1){
+            width++;
+        }
+
+        // generate an image of the appropriate size
+        GreenfootImage resultImage = new GreenfootImage(width, height);
+
+        // fill in the background colour
+        resultImage.setColor (back);
+        resultImage.fill();
+
+        // Draw the border
+        resultImage.setColor (border); 
+        resultImage.drawRect (0, 0, width-1, height-1);
+
+        
+        
+        // Done!
+        return resultImage;
+    }
+    
+    public void deleteDescription() {
+        
+    }
+    
+    public void createDescription() {
+        
+    }
 }
