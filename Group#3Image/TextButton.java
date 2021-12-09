119bbdfea8f8fd2cf2f5c6b7a2d27de86967f7b8
Matthew <m4ttp0w3r@gmail.com>
2021-12-09 15:09:51 -0500

    Added Popups
    Not all descriptions are written yet

--- Group#3Image/TextButton.java
+++ Group#3Image/TextButton.java
@@ -28,6 +28,7 @@ public class TextButton extends Control
     private Color highlightedBackColor;
     private Color disabledColor;
     private Color borderColor;
+    private SuperTextBox description;
 
     private Font font;
 
@@ -95,6 +96,8 @@ public class TextButton extends Control
         buttonText = text;
         active = true;
 
+        description = new SuperTextBox(getDescriptionText(), Color.DARK_GRAY, Color.WHITE, new Font("Comic Sans MS", false, false, 13), true, 160, 1, Color.WHITE);
+        
         update();
         setImage(myImage);
     }
@@ -206,4 +209,22 @@ public class TextButton extends Control
         return resultImage;
     }
 
+    public void createDescription(){
+        (getWorld()).addObject(description, getX(), getY() + 60);
+    }
+    
+    public void deleteDescription(){
+        (getWorld()).removeObject(description);
+    }
+    
+    public String[] getDescriptionText(){
+        switch(buttonText){
+            case "Undo":
+                return new String[]{"Returns the image", " to the state ", "before the last edit"};
+            case "Redo":
+                return new String[]{"Returns the image", " to the state ", "before the last undo"};
+            default:
+                return new String[]{"Unfinished"};
+        }
+    }
 }
